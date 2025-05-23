58a70ad75018dc61edb93c1e5f04074fdc5b6e12
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-05-23 12:04:02 +0100

    Stop player from moving into wall

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: g24force <g24force@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 13:45:15 by gjose-fr          #+#    #+#             */
-/*   Updated: 2025/05/23 10:10:43 by g24force         ###   ########.fr       */
+/*   Updated: 2025/05/23 12:00:05 by g24force         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	move_right(t_map *map)
 	t_player	*player;
 
 	player = &map->player;
+	if (map->matrix[player->x_coord][player->y_coord + 1] == '1')
+		return;
 	map->matrix[player->x_coord][player->y_coord] = '0';
 	player->y_coord += 1;
 	map->matrix[player->x_coord][player->y_coord] = 'P';
@@ -28,6 +30,8 @@ void	move_down(t_map *map)
 	t_player	*player;
 
 	player = &map->player;
+	if (map->matrix[player->x_coord + 1][player->y_coord] == '1')
+		return;
 	map->matrix[player->x_coord][player->y_coord] = '0';
 	player->x_coord += 1;
 	map->matrix[player->x_coord][player->y_coord] = 'P';
@@ -38,6 +42,8 @@ void	move_left(t_map *map)
 	t_player	*player;
 
 	player = &map->player;
+	if (map->matrix[player->x_coord][player->y_coord - 1] == '1')
+		return;
 	map->matrix[player->x_coord][player->y_coord] = '0';
 	player->y_coord -= 1;
 	map->matrix[player->x_coord][player->y_coord] = 'P';
@@ -48,6 +54,8 @@ void	move_up(t_map *map)
 	t_player	*player;
 
 	player = &map->player;
+	if (map->matrix[player->x_coord - 1][player->y_coord] == '1')
+		return;
 	map->matrix[player->x_coord][player->y_coord] = '0';
 	player->x_coord -= 1;
 	map->matrix[player->x_coord][player->y_coord] = 'P';
