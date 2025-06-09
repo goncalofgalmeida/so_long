bf863c087250f793686c65216bbd060746b6c0d1
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-06-09 18:40:23 +0100

    Init structs before using

--- srcs/init.c
+++ srcs/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: g24force <g24force@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gjose-fr <gjose-fr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 16:04:30 by gjose-fr          #+#    #+#             */
-/*   Updated: 2025/06/06 23:14:51 by g24force         ###   ########.fr       */
+/*   Updated: 2025/06/09 18:01:48 by gjose-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,18 @@ void	init_player_coords(t_player *player)
 	player->move_count = 0;
 }
 
+void	init_sprites_struct(t_sprites *sprites)
+{
+	sprites->floor = NULL;
+	sprites->wall = NULL;
+	sprites->exit = NULL;
+	sprites->col = NULL;
+	sprites->p_up = NULL;
+	sprites->p_right = NULL;
+	sprites->p_down = NULL;
+	sprites->p_left = NULL;
+}
+
 void	init_maps(t_map *map)
 {
 	map->matrix = NULL;
@@ -27,4 +39,17 @@ void	init_maps(t_map *map)
 	init_player_coords(&(map->player));
 	map->height = 0;
 	map->width = 0;
+}
+
+void	init_mlx_struct(t_data *mlx)
+{
+	mlx->init = NULL;
+	mlx->window = NULL;
+}
+
+void	init_structs(t_game *game)
+{
+	init_maps(&(game->map));
+	init_mlx_struct(&(game->mlx));
+	init_sprites_struct(&(game->mlx.sprites));
 }
